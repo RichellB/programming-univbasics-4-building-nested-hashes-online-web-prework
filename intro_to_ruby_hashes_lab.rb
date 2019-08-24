8c0c6dbdcb2ab65b21ba080ce2154ad04ce84e96
Richell Bonfield <richell.bonfield12@gmail.com>
2019-08-24 19:57:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,7 +26,7 @@ def monopoly_with_third_tier
   
    food = {
 	  :railroads => {pieces: 4, :rent_in_dollars => {
-	    name: "", age: "", weight: "", height: ""}
+	    name: "", age: "", weight: "", height: ""},
 	    :names => {
 	      name1: "", name2: "", name3: "", name4: ""}
 	  }
